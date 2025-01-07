7693540edc96405de771c2c3a951c84e1f179daa
Serban Ghita <serbanghita@gmail.com>
2025-01-07 16:47:03 +0200

    Correct paths

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,14 @@ export default defineConfig({
     plugins: [tsconfigPaths()],
     test: {
         alias: {
-            '@serbanghita-gamedev/assets/': path.join(__dirname,  '../assets/'),
-            '@serbanghita-gamedev/bitmask/': path.join(__dirname,  '../bitmask/'),
-            '@serbanghita-gamedev/component/': path.join(__dirname,  '../component/'),
-            '@serbanghita-gamedev/ecs/': path.join(__dirname,  '../ecs/'),
-            '@serbanghita-gamedev/input/': path.join(__dirname,  '../input/'),
-            '@serbanghita-gamedev/renderer/': path.join(__dirname,  '../renderer/'),
-            '@serbanghita-gamedev/matrix/': path.join(__dirname,  '../matrix/'),
-            '@serbanghita-gamedev/tiled/': path.join(__dirname,  '../tiled/')
+            '@serbanghita-gamedev/assets/': path.join(__dirname,  '../gamedev/packages/assets/'),
+            '@serbanghita-gamedev/bitmask/': path.join(__dirname,  '../gamedev/packages/bitmask/'),
+            '@serbanghita-gamedev/component/': path.join(__dirname,  '../gamedev/packages/component/'),
+            '@serbanghita-gamedev/ecs/': path.join(__dirname,  '../gamedev/packages/ecs/'),
+            '@serbanghita-gamedev/input/': path.join(__dirname,  '../gamedev/packages/input/'),
+            '@serbanghita-gamedev/renderer/': path.join(__dirname,  '../gamedev/packages/renderer/'),
+            '@serbanghita-gamedev/matrix/': path.join(__dirname,  '../gamedev/packages/matrix/'),
+            '@serbanghita-gamedev/tiled/': path.join(__dirname,  '../gamedev/packages/tiled/')
         }
     }
 })
